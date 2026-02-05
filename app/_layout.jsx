659e0971428818087202478f5419fbe5a959a8d6
Alperen Yanık <alperenyanik@hotmail.com>
2026-02-05 14:30:18 +0300

    feat: created project

---
+++ app/_layout.jsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+
+export default function RootLayout() {
+
+  return (
+    <>
+      <StatusBar value="auto" />
+      <Stack
+      >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  );
+}
