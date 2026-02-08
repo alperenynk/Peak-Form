9ff3cbb49ed534f67fb42b8c75f06f394b4bf519
Alperen Yanık <alperenyanik@hotmail.com>
2026-02-08 15:18:09 +0300

    refactor: updated asset paths
    Replace the generic icon with dedicated muscle images. Updated favourites.jsx and search.jsx to require specific files from assets/images (biceps, triceps, chest, back, quadriceps, shoulder, abs, forearm, calves, hamstrings, glutes, neck).

--- app/(tabs)/search.jsx
+++ app/(tabs)/search.jsx
@@ -24,18 +24,18 @@ const { width } = Dimensions.get("window");
 
 export default function Search() {
   const muscleGroups = [
-    { id: "1", title: "Biceps", image: require("../../assets/icon.png") },
-    { id: "2", title: "Triceps", image: require("../../assets/icon.png") },
-    { id: "3", title: "Chest", image: require("../../assets/icon.png") },
-    { id: "4", title: "Back", image: require("../../assets/icon.png") },
-    { id: "5", title: "Quadriceps", image: require("../../assets/icon.png") },
-    { id: "6", title: "Shoulders", image: require("../../assets/icon.png") },
-    { id: "7", title: "Abs", image: require("../../assets/icon.png") },
-    { id: "8", title: "Forearms", image: require("../../assets/icon.png") },
-    { id: "9", title: "Calves", image: require("../../assets/icon.png") },
-    { id: "10", title: "Hamstrings", image: require("../../assets/icon.png") },
-    { id: "11", title: "Glutes", image: require("../../assets/icon.png") },
-    { id: "12", title: "Neck", image: require("../../assets/icon.png") },
+    { id: "1", title: "Biceps", image: require("../../assets/images/biceps.png") },
+    { id: "2", title: "Triceps", image: require("../../assets/images/triceps.png") },
+    { id: "3", title: "Chest", image: require("../../assets/images/chest.png") },
+    { id: "4", title: "Back", image: require("../../assets/images/back.png") },
+    { id: "5", title: "Quadriceps", image: require("../../assets/images/quadriceps.png") },
+    { id: "6", title: "Shoulders", image: require("../../assets/images/shoulder.png") },
+    { id: "7", title: "Abs", image: require("../../assets/images/abs.png") },
+    { id: "8", title: "Forearms", image: require("../../assets/images/forearm.png") },
+    { id: "9", title: "Calves", image: require("../../assets/images/calves.png") },
+    { id: "10", title: "Hamstrings", image: require("../../assets/images/hamstrings.png") },
+    { id: "11", title: "Glutes", image: require("../../assets/images/glutes.png") },
+    { id: "12", title: "Neck", image: require("../../assets/images/neck.png") },
   ];
 
   const insets = useSafeAreaInsets();
@@ -270,7 +270,7 @@ const styles = StyleSheet.create({
   favouriteButton: {
     position: "absolute",
     top: 12,
-    left: 12,
+    right: 12,
     zIndex: 10,
     padding: 6,
     borderRadius: 20,
