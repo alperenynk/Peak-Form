d746be94cf065437cec02e200aba685f3dc84be9
Alperen Yanık <alperenyanik@hotmail.com>
2026-02-15 14:47:58 +0300

    refactor: updated gifs and paths

--- src/data/muscleData.js
+++ src/data/muscleData.js
@@ -62,64 +62,75 @@ export const muscleGroups = [
 ];
 
 export const muscleGifs = {
-//   "1": [
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//   ],
-//   "2": [
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//   ],
-  "3": [
-    require("../../assets/gifs/chest.gif"),
-    require("../../assets/gifs/chest.gif"),
-    require("../../assets/gifs/chest.gif"),
+  "1": [
+    require("../../assets/gifs/biceps/biceps-1.gif"),
+    require("../../assets/gifs/biceps/biceps-2.gif"),
+    // require("../../assets/gifs/biceps/.gif"),
   ],
-//   "4": [
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//   ],
-//   "5": [
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//   ],
-//   "6": [
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//   ],
-//   "7": [
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//   ],
-//   "8": [
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//   ],
-//   "9": [
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//   ],
-//   "10": [
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//   ],
-//   "11": [
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//   ],
-//   "12": [
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//     require("../../assets/gifs/.gif"),
-//   ],
+  "2": [
+    require("../../assets/gifs/triceps/triceps-1.gif"),
+    require("../../assets/gifs/triceps/triceps-2.gif"),
+    require("../../assets/gifs/triceps/triceps-3.gif"),
+    require("../../assets/gifs/triceps/triceps-4.gif"),
+  ],
+  "3": [
+    require("../../assets/gifs/chest/chest-1.gif"),
+    require("../../assets/gifs/chest/chest-2.gif"),
+    require("../../assets/gifs/chest/chest-3.gif"),
+    require("../../assets/gifs/chest/chest-4.gif"),
+    require("../../assets/gifs/chest/chest-5.gif"),
+  ],
+  "4": [
+    require("../../assets/gifs/back/back-1.gif"),
+    require("../../assets/gifs/back/back-2.gif"),
+    require("../../assets/gifs/back/back-3.gif"),
+  ],
+  "5": [
+    require("../../assets/gifs/quadriceps/quadriceps-1.gif"),
+    require("../../assets/gifs/quadriceps/quadriceps-2.gif"),
+    require("../../assets/gifs/quadriceps/quadriceps-3.gif"),
+    require("../../assets/gifs/quadriceps/quadriceps-4.gif"),
+    require("../../assets/gifs/quadriceps/quadriceps-5.gif"),
+  ],
+  "6": [
+    require("../../assets/gifs/shoulders/shoulders-1.gif"),
+    require("../../assets/gifs/shoulders/shoulders-2.gif"),
+    require("../../assets/gifs/shoulders/shoulders-3.gif"),
+    require("../../assets/gifs/shoulders/shoulders-4.gif"),
+  ],
+  "7": [
+    require("../../assets/gifs/abs/abs-1.gif"),
+    require("../../assets/gifs/abs/abs-2.gif"),
+    require("../../assets/gifs/abs/abs-3.gif"),
+    require("../../assets/gifs/abs/abs-4.gif"),
+    require("../../assets/gifs/abs/abs-5.gif"),
+  ],
+  "8": [
+    require("../../assets/gifs/forearms/forearms-1.gif"),
+    require("../../assets/gifs/forearms/forearms-2.gif"),
+    require("../../assets/gifs/forearms/forearms-3.gif"),
+  ],
+  "9": [
+    require("../../assets/gifs/calves/calves-1.gif"),
+    require("../../assets/gifs/calves/calves-2.gif"),
+    // require("../../assets/gifs/calves/.gif"),
+  ],
+  "10": [
+    require("../../assets/gifs/hamstrings/hamstrings-1.gif"),
+    require("../../assets/gifs/hamstrings/hamstrings-2.gif"),
+    require("../../assets/gifs/hamstrings/hamstrings-3.gif"),
+    require("../../assets/gifs/hamstrings/hamstrings-4.gif"),
+    require("../../assets/gifs/hamstrings/hamstrings-5.gif"),
+  ],
+  "11": [
+    require("../../assets/gifs/glutes/glutes-1.gif"),
+    require("../../assets/gifs/glutes/glutes-2.gif"),
+    require("../../assets/gifs/glutes/glutes-3.gif"),
+    require("../../assets/gifs/glutes/glutes-4.gif"),
+  ],
+  "12": [
+    require("../../assets/gifs/neck/neck-1.gif"),
+    require("../../assets/gifs/neck/neck-2.gif"),
+    require("../../assets/gifs/neck/neck-3.gif"),
+  ],
 };
